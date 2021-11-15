9920cbdd988383fa8d5f215457faaa0563d128e9
Nicolas Filzi <nfilzi.webservices@gmail.com>
2021-11-15 18:59:11 +0100

    Finish up flats show feature
    Jazz it up with le wagon uikit + bootstrap stuff

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,5 +7,9 @@ class FlatsController < ApplicationController
   end
 
   def show
+    url   = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    flats = JSON.parse(open(url).read)
+
+    @flat = flats.find { |flat| flat["id"] == params[:id].to_i }
   end
 end
